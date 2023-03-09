f972c7a25e6259c24b8f4ce93f76626ee771ed06
Gert Timmerman <gtimmerman@fritsjurgens.com>
2023-03-09 16:35:17 +0100

    Read files from the asset container

--- src/Listeners/PushToWebhook.php
+++ src/Listeners/PushToWebhook.php
@@ -3,6 +3,7 @@
 namespace GertTimmerman\StatamicZapier\Listeners;
 
 use GuzzleHttp\Client;
+use Statamic\Facades\Asset;
 use Statamic\Events\FormSubmitted;
 use Illuminate\Support\Facades\Http;
 use GertTimmerman\StatamicZapier\Webhooks;
@@ -35,22 +36,44 @@ class PushToWebhook
         $data = $event->submission->data();
         $data['submission_id'] = $event->submission->id();
 
+        // dd($event->submission->data(), $event->submission->fields());
 
         // filter out the attachments
         $attachmentsFields = $event->submission->fields()->filter(function ($value, $key) use ($data) {
             return is_array($value) && isset($value['type']) && $value['type'] == "assets" && isset($data[$key]) && $data[$key] != null;
-        });
+        })->keys()->all();
 
         $multipartBody = [];
 
-        foreach($attachmentsFields as $handle => $value) {
-            $file = request()->file($handle);
-            $multipartBody[] = [
-                'name' => $handle,
-                'contents' => $file->get(),
-                'filename' => $file->getClientOriginalName(),
-                'Content-type' => 'multipart/form-data'
-            ];
+        foreach($attachmentsFields as $handle) {
+            $formField = $event->submission->fields()->get($handle);
+            $assetContainer = $formField['container'];
+
+            $files = $event->submission->data()->get($handle);
+
+            if (!is_array($files)) {
+                $files[] = $files;
+            }
+
+            foreach($files as $file) {
+                $asset = Asset::query()
+                    ->where('container', $assetContainer)
+                    ->where('path', 'like', '%' . $file . '%')
+                    ->get();
+
+                if (is_null($asset)) continue;
+
+                // get the first one, because we just need one asset
+                $asset = $asset[0];
+
+                $multipartBody[] = [
+                    'name' => $handle,
+                    'contents' => $asset->contents(),
+                    'filename' => $asset->basename(),
+                    'Content-type' => 'multipart/form-data'
+                ];
+            }
+            
             unset($data[$handle]);
         }
 
